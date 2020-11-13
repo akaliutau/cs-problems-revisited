be1f80d53ae703db0a016171f5c102b1c811e698
Aliaksei Kaliutau <k5771k@gmail.com>
2020-11-13 10:16:18 +0300

    updated

--- src/main/java/com/problems/stack/Solution456.java
+++ src/main/java/com/problems/stack/Solution456.java
@@ -16,7 +16,26 @@ import java.util.Stack;
  * the sequence.
  * 
  * IDEA:
+ * [1,4,5,2]
  * 
+ * look on the stack in 2 directions
+ * for each i
+ * one direction
+ *  -->
+ * min number seen so far TO THE LEFT of i - check out min[i]
+ * 
+ * <--
+ * use filtered stack which contains:
+ * 1) values bigger than min[i]
+ * 
+ * finally we have:
+ * 
+ * nums  1   4  5  2
+ * min   1   1  1  1
+ * stack           2
+ *       1    < 5> 2     
+ *  
+ *  
  * 
  */
 public class Solution456 {
@@ -42,7 +61,7 @@ public class Solution456 {
 					stackKthElem.pop();
 				}
 				// stackKthElem is 1) empty OR 2) contains elem > min0j
-				if (!stackKthElem.isEmpty() && stackKthElem.peek() < nums[j]) {// min0j < stackKthElem < nums[j], guaranteed that k > j
+				if (!stackKthElem.isEmpty() && nums[j] > stackKthElem.peek()) {// min0j < stackKthElem < nums[j], guaranteed that k > j
 					return true;
 				}
 				stackKthElem.push(nums[j]);
