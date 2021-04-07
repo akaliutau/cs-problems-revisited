bca120c8b27102f40e26003884d2d452db25590b
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2021-04-07 16:30:10 +0300

    added solutions

---
+++ src/main/java/com/problems/misc/Solution1114.java
@@ -0,0 +1,51 @@
+package com.problems.misc;
+
+/**
+ * Suppose we have a class:
+ * 
+ * public class Foo { public void first() { print("first"); } public void
+ * second() { print("second"); } public void third() { print("third"); } } The
+ * same instance of Foo will be passed to three different threads. Thread A will
+ * call first(), thread B will call second(), and thread C will call third().
+ * Design a mechanism and modify the program to ensure that second() is executed
+ * after first(), and third() is executed after second().
+ *
+ * 
+ */
+public class Solution1114 {
+	static class Foo {
+	    
+	    private volatile boolean[] printed = new boolean[3];
+
+	    public Foo() {
+	        
+	    }
+
+	    synchronized public void first(Runnable printFirst) throws InterruptedException {
+	        
+	        printed[0] = true;
+	        printFirst.run();
+	        notifyAll();
+	    }
+
+	    synchronized public void second(Runnable printSecond) throws InterruptedException {
+	        while (!printed[0]){
+	            wait();
+	        }
+	        printed[1] = true;
+	        
+	        printSecond.run();
+	        notifyAll();
+	    }
+
+	    synchronized public void third(Runnable printThird) throws InterruptedException {
+	        while (!printed[0] || !printed[1]){
+	            wait();
+	        }
+	        printed[2] = true;
+	        
+	        printThird.run();
+	        notifyAll();
+	    }
+	}
+}
---
+++ src/main/java/com/problems/string/Solution1023.java
@@ -0,0 +1,71 @@
+package com.problems.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A query word matches a given pattern if we can insert lowercase letters to
+ * the pattern word so that it equals the query. (We may insert each character
+ * at any position, and may insert 0 characters.)
+ * 
+ * Given a list of queries, and a pattern, return an answer list of booleans,
+ * where answer[i] is true if and only if queries[i] matches the pattern.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: queries =
+ * ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern =
+ * "FB" Output: [true,false,true,true,false] Explanation: "FooBar" can be
+ * generated like this "F" + "oo" + "B" + "ar". "FootBall" can be generated like
+ * this "F" + "oot" + "B" + "all". "FrameBuffer" can be generated like this "F"
+ * + "rame" + "B" + "uffer".
+ * 
+ * IDEA:
+ * find all occurrences of symbols from pattern in string, letter by letter
+ * Note the additional checks for Uppercase letters on the tail and between
+ *
+ */
+public class Solution1023 {
+
+	static boolean match(String s, char[] pattern) {
+		char[] lets = s.toCharArray();
+		int i = 0;
+		int pi = 0;
+		while (pi < pattern.length && i < lets.length) {
+			boolean found = false;
+			while (i < lets.length) {
+				char c = lets[i++];
+				if (c == pattern[pi]) {
+					found = true;
+					break;
+				}
+				if (Character.isUpperCase(c)) {
+					return false;
+				}
+			}
+			if (!found) {
+				return false;
+			}
+			pi++;// advance to the next symbol
+		}
+		// check the tail of string against upper case letters
+		while (i < lets.length) {
+			if (Character.isUpperCase(lets[i++])) {
+				return false;
+			}
+		}
+		
+		return pi == pattern.length;
+	}
+
+	public List<Boolean> camelMatch(String[] queries, String pattern) {
+		char[] p = pattern.toCharArray();
+		List<Boolean> ans = new ArrayList<>();
+		for (String query : queries) {
+			ans.add(match(query, p));
+		}
+		return ans;
+	}
+}
