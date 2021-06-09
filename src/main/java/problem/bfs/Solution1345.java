55c7135d4357324a1aabb69b5e5250083c0f28f3
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2021-06-09 14:44:56 +0300

    updated solutions

--- src/main/java/problem/bfs/Solution1345.java
+++ src/main/java/problem/bfs/Solution1345.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -14,8 +15,11 @@ import java.util.Set;
  * 
  * In one step you can jump from index i to index:
  * 
- * i + 1 where: i + 1 < arr.length. i - 1 where: i - 1 >= 0. j where: arr[i] ==
- * arr[j] and i != j. Return the minimum number of steps to reach the last index
+ * i + 1 where: i + 1 < arr.length. 
+ * i - 1 where: i - 1 >= 0. 
+ * j where: arr[i] == arr[j] and i != j. 
+ * 
+ * Return the minimum number of steps to reach the last index
  * of the array.
  * 
  * Notice that you can not jump outside of the array at any time.
@@ -23,41 +27,47 @@ import java.util.Set;
  * Example 1:
  * 
  * Input: arr = [100,-23,-23,404,100,23,23,23,3,404] Output: 3
+ * 
+ * IDEA:
+ * 1. use numbers as nodes and BFS to find the fastest route
+ * 2. start propagation process from each node
+ * 3. use mapping // node_value => [list of refs] to access fast routes
  *
  */
 public class Solution1345 {
 	public int minJumps(int[] arr) {
 		int n = arr.length;
-		if (n <= 1) {
+		if (n <= 1) {// edge case
 			return 0;
 		}
-
-		Map<Integer, List<Integer>> graph = new HashMap<>();
+		// preparations
+		Map<Integer, List<Integer>> graph = new HashMap<>();// node_value => [list of node Ids]
 		for (int i = 0; i < n; i++) {
 			graph.computeIfAbsent(arr[i], v -> new LinkedList<>()).add(i);
 		}
 
-		List<Integer> curs = new LinkedList<>(); // store current layer
-		curs.add(0);
+		Queue<Integer> q = new LinkedList<>(); // store current layer
+		q.add(0);
 		Set<Integer> visited = new HashSet<>();
 		int step = 0;
 
 		// when current layer exists
-		while (!curs.isEmpty()) {
-			List<Integer> nex = new LinkedList<>();
+		while (!q.isEmpty()) {
+			List<Integer> next = new LinkedList<>();
 
 			// iterate the layer
-			for (int node : curs) {
+			for (int node : q) {
 				// check if reached end
 				if (node == n - 1) {
 					return step;
 				}
 
 				// check same value
-				for (int child : graph.get(arr[node])) {
+				List<Integer> connected = graph.get(arr[node]); 
+				for (int child : connected) {
 					if (!visited.contains(child)) {
 						visited.add(child);
-						nex.add(child);
+						next.add(child);
 					}
 				}
 
@@ -67,18 +77,17 @@ public class Solution1345 {
 				// check neighbors
 				if (node + 1 < n && !visited.contains(node + 1)) {
 					visited.add(node + 1);
-					nex.add(node + 1);
+					next.add(node + 1);
 				}
 				if (node - 1 >= 0 && !visited.contains(node - 1)) {
 					visited.add(node - 1);
-					nex.add(node - 1);
+					next.add(node - 1);
 				}
 			}
-
-			curs = nex;
+            q.clear();
+			q.addAll(next);
 			step++;
 		}
-
 		return -1;
 	}
 }
--- src/main/java/problem/priorityqueue/Solution1696a.java
+++ src/main/java/problem/priorityqueue/Solution1696a.java
@@ -1,6 +1,6 @@
 package problem.priorityqueue;
 
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 /**
  * You are given a 0-indexed integer array nums and an integer k.
@@ -27,30 +27,27 @@ import java.util.PriorityQueue;
  * 7.
  * 
  * IDEA:
- * the most optimal sequence is that consisting from the biggest elements
- *  * 
- * [1,-1,-2,4,-7,3]
- *          |
- *    {   }look up the biggest elem in the range [i-k, i-1]
+ * NOTE: this solution is not optimal one (results in TLE error)
+ * 
+ *   [1,-1,-2,4,-7,3]  
+ *       0 -1
+ *         -1 4
+ *            4 -8
+ *              -3 7    
  */
-public class Solution1696 {
+public class Solution1696a {
 	public int maxResult(int[] nums, int k) {
 		int n = nums.length;
 		int[] dp = new int[n];
+		Arrays.fill(dp, Integer.MIN_VALUE);
+		if (k == 0) {
+			return 0;
+		}
 		dp[0] = nums[0];
-		
-		// used to hold containers [(num), (its index)], where biggest ones are on the top
-		// contains the biggest numbers in path seen so far
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o, p) -> p[0] - o[0]);
-		pq.add(new int[] { nums[0], 0 });
-		
-		for (int i = 1; i < n; i++) {
-			int tooFarTh = i - k; 
-			while (pq.peek()[1] < tooFarTh) {// clean up queue from too distant elements
-				pq.remove();
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j <= i + k && j < n; j++) {
+				dp[j] = Math.max(dp[j], nums[j] + dp[i]);
 			}
-			dp[i] = nums[i] + dp[pq.peek()[1]];
-			pq.add(new int[] { dp[i], i });
 		}
 		return dp[n - 1];
 	}
